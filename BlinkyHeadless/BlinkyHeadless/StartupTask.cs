71b6b41258c7e409fda526781957a33c8f11b793
tutunkommon <tutunkommon@live.com>
2017-04-23 18:41:27 -0400

    First blinky light

---
+++ BlinkyHeadless/BlinkyHeadless/StartupTask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.System.Threading;
+using Windows.Devices.Gpio;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace BlinkyHeadless
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        BackgroundTaskDeferral _deferral;
+        private const int LED_PIN = 5;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        private ThreadPoolTimer timer;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _deferral = taskInstance.GetDeferral();
+
+            InitGPIO();
+
+            timer = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick,
+                       TimeSpan.FromMilliseconds(500));
+        }
+
+        private void Timer_Tick(ThreadPoolTimer timer)
+        {
+            if (GpioPinValue.High == pinValue)
+                pinValue = GpioPinValue.Low;
+            else
+                pinValue = GpioPinValue.High;
+
+            pin.Write(pinValue);
+        }
+
+        private void InitGPIO()
+        {
+            GpioController gpio = GpioController.GetDefault();
+            pin = gpio.OpenPin(LED_PIN);
+            pinValue = GpioPinValue.High;
+            pin.Write(pinValue);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+    }
+}
